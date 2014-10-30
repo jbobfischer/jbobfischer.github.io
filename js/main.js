4194e41894bf53ee03869d693099951ebfa34ec7
jbobfischer <jessefischer@gmail.com>
2014-10-30 08:10:16 -0400

    triggering a build

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
+$(document).ready(function($) {
+				// init controller
+				var controller = new ScrollMagic({globalSceneOptions: {duration: 100}});
 
+				// build scenes
+				new ScrollScene({triggerElement: "#sec1"})
+								.setClassToggle("#high1", "active") // add class toggle
+								.addTo(controller)
+								.addIndicators();
+				new ScrollScene({triggerElement: "#sec2"})
+								.setClassToggle("#high2", "active") // add class toggle
+								.addTo(controller)
+								.addIndicators();
+				new ScrollScene({triggerElement: "#sec3"})
+								.setClassToggle("#high3", "active") // add class toggle
+								.addTo(controller)
+								.addIndicators();
+				new ScrollScene({triggerElement: "#sec4"})
+								.setClassToggle("#high4", "active") // add class toggle
+								.addTo(controller)
+								.addIndicators();
+
+			});
